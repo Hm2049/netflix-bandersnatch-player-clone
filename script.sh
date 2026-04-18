5174c2f08df9fad0551c7a134ee66d956ae0e2e6
Helton Muniz <heltonmuniz2049@gmail.com>
2026-04-18 06:29:15 -0300

    fix(scripts): fix ffmpeg script for assets

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@ for mediaFile in `ls $ASSETSFOLDER | grep .mp4`; do
     FILENAME=$(echo $mediaFile | sed -n 's/.mp4//p' | sed -n 's/-1920x1080//p')
     INPUT=$ASSETSFOLDER/$mediaFile
     FOLDER_TARGET=$ASSETSFOLDER/$FILENAME
-    mkdir -p $FOLDER_TARGET/$FILENAME
+    mkdir -p "$FOLDER_TARGET"
 
     # criar arquivos de resolucoes diferentes na pasta
-    OUTPUT=$FOLDER_TARGET/$FILENAME/$FILENAME
+    OUTPUT="$FOLDER_TARGET/$FILENAME"
     DURATION=$(ffprobe -i $INPUT -show_format -v quiet | sed -n 's/duration=//p')
     
     OUTPUT144=$OUTPUT-$DURATION-144
